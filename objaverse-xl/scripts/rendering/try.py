31ee7a638cf7217c39352b4c165d432822562d63
dhmbb2 <yjqsjtu2022@sjtu.edu.cn>
2025-12-07 20:21:30 +0800

    remove objaverse dir

--- objaverse-xl/scripts/rendering/try.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# 打开 PNG 图片
-image = Image.open("/inspire/hdd/global_user/chenxinyan-240108120066/yihang/LVSM/objaverse-xl/scripts/rendering/data_rendered/000-000/00a1a602456f4eb188b522d7ef19e81b/000.png")
-
-# 获取图片的分辨率（宽度和高度）
-width, height = image.size
-
-print(f"图片分辨率: {width}x{height}")
